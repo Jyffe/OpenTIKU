0ddba4bafee2fc5621b342da477b227466f8528c
Jyrki Rantonen <jyrki.rantonen@tieto.com>
2017-10-10 15:52:09 +0300

    Trying out Websockets with a small chat-service and client, something that might stay in the PoC also in the future.

---
+++ sa-poc-event-websocket-service/src/main/java/fi/jyffe/sa/poc/event_websocket_service/Message.java
@@ -0,0 +1,35 @@
+package fi.jyffe.sa.poc.event_websocket_service;
+
+public class Message
+{
+    private String from;
+    private String text;
+
+    public Message() {}
+
+    public Message(String from,String text)
+    {
+	this.from = from;
+	this.text = text;
+    }
+
+    public String getFrom()
+    {
+        return from;
+    }
+
+    public void setFrom(String from)
+    {
+        this.from = from;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+}
