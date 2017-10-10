594daadfb4e49d99e11124db391871f82586b570
Jyrki Rantonen <jyrki.rantonen@tieto.com>
2017-10-10 11:52:51 +0300

    Some missing bits and pieces…

--- sa-poc-issue-rest-service/src/main/java/fi/jyffe/sa/poc/issue_rest_service/IssueController.java
+++ sa-poc-issue-rest-service/src/main/java/fi/jyffe/sa/poc/issue_rest_service/IssueController.java
@@ -1,6 +1,22 @@
 package fi.jyffe.sa.poc.issue_rest_service;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+/*
+ *  @RestController annotation marks the class as a REST controller where every method returns a domain object instead of a server
+ *  side rendered view (as in case of a MVC controller)
+ */
+@RestController
 public class IssueController {
 
+	/*	Handles the HTTP GET request for /issue, i.e. returns HTTP 200 OK with a JSON representation of an Issue in the response body.
+	 *  
+	 *  TODO: Add mapping for all the HTTP operations (GET, PUT, POST, ...) and logic for handling those
+	 */
+    @RequestMapping("/issue")
+    public Issue issue(@RequestParam(value="type", defaultValue="No type") String type) {
+        return new Issue(type);
     }
+}
