c0337fbfe978db270a005021efab51ebd9b93bde
Jyrki Rantonen <jyrki.rantonen@tieto.com>
2017-11-24 20:11:18 +0200

    Cleaning up and commenting

--- sa-poc-firealarm-integration/src/main/java/fi/jyffe/sa/poc/firealarm_integration_service/App.java
+++ sa-poc-firealarm-integration/src/main/java/fi/jyffe/sa/poc/firealarm_integration_service/App.java
@@ -9,19 +9,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 
+ * @author Jyrki Rantonen
+ * 
+ * SA PoC Firealarm Integration Microservice
+ * 
+ * Proof-of-concept for a open situational awareness software - OpenTIKU
+ * 
+ * Demonstrates a generic service that integrates with an external subsystem (firealarm system in this case)
+ * and produces incoming events (i.e. state changes) as messages over Kafka -topic.
+ * 
+ */
 @SpringBootApplication
 public class App 
 {	
+	/* Being lazy here for now... instead of doing a proper integration I have just 
+	 * 
+	 * TODO: Have a proper interface and integration instead of all this. Web Services/SOAP perhaps?
+	 * 
+	 */
     public static void main( String[] args ) throws InterruptedException
     {
+    		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    		
     		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
     		
     		MessageProducer producer = context.getBean(MessageProducer.class);
-
-    		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-
-    		//EventMessage msg = new EventMessage();
     		
+    		/* Keep sending the message and increment the id once a second so that we just see something 
+    		going through the system...
+    		*/
     		executorService.scheduleAtFixedRate(new Runnable() {
     			
     			Integer i = 0;
@@ -29,8 +47,8 @@ public class App
     			@Override
     	        public void run() {
     				
-    				//producer.sendMessage("Alarm "+i++);
     				producer.send(new EventMessage(i.toString(), "status"));
+    				
     				i++;
     	        }
     	    }, 0, 1, TimeUnit.SECONDS);
