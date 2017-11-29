2f28ecb0acceb71577759f0a58e55710f3618ea1
Jyrki Rantonen <jyrki.rantonen@gmail.com>
2017-11-29 18:57:25 +0200

    Refactoring...

--- opentiku-web-app/src/main/java/fi/jyffe/opentiku/webapp/App.java
+++ opentiku-web-app/src/main/java/fi/jyffe/opentiku/webapp/App.java
@@ -1,4 +1,4 @@
-package fi.jyffe.sa.poc.web_app;
+package fi.jyffe.opentiku.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
