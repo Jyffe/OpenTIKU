a3112fe687b6c4e6cce9b804e75dee52db7e4ff7
Jyrki Rantonen <jyrki.rantonen@gmail.com>
2017-11-29 19:04:58 +0200

    Comments and stuff

---
+++ opentiku-web-app/README.md
@@ -0,0 +1,5 @@
+# Open TIKU Web App
+
+To be the Open TIku Web App - no next to non-existing and just for receiving simple Websocket events.
+
+See [Open TIKU Wiki](https://github.com/Jyffe/OpenTIKU/wiki) for more details.
--- opentiku-web-app/src/main/java/fi/jyffe/opentiku/webapp/App.java
+++ opentiku-web-app/src/main/java/fi/jyffe/opentiku/webapp/App.java
@@ -3,6 +3,17 @@ package fi.jyffe.opentiku.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Jyrki Rantonen
+ * 
+ * Open TIKU Web App
+ * 
+ * Proof-of-concept for a open situational awareness software - OpenTIKU
+ * 
+ * Front-end for Angular Web App - using Spring Boot and Tomcat for now, need to look into this later on... 
+ * 
+ */
 @SpringBootApplication
 public class App 
 {
