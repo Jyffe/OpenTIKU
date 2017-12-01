4911df5c04d7dc3f48821c76d44813d5dd43b0e2
Jyrki Rantonen <jyrki.rantonen@gmail.com>
2017-12-01 15:47:38 +0200

    Skeleton fot REST controller

---
+++ opentiku-firealarm-integration/src/main/java/fi/jyffe/opentiku/integration/firealarm/rest/RestEventController.java
@@ -0,0 +1,21 @@
+package fi.jyffe.opentiku.integration.firealarm.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author Jyrki Rantonen
+ * 
+ * TOBE REST Controller
+ * http://www.baeldung.com/rest-with-spring-series/
+ * https://spring.io/guides/tutorials/bookmarks/
+ * https://dzone.com/articles/creating-a-rest-api-with-java-and-spring
+ *
+ */
+@RestController
+public class RestEventController {
+
+
+}
---
+++ opentiku-firealarm-integration/src/main/java/fi/jyffe/opentiku/integration/firealarm/rest/RestEventDTO.java
@@ -0,0 +1,58 @@
+package fi.jyffe.opentiku.integration.firealarm.rest;
+
+public class RestEventDTO {
+	// Unique identifer of target target (device, sensor, machine, vehicle, ...) that the received event is about
+			private String id;
+			
+			// Status of the target (on, off, running, stopping, stopped, starting, alarming, ...)
+			private String state;
+			
+			// Location of the target in case it does not have a fixed location (e.g. moving vehicle) or it is otherwise known
+			// by the sender
+			private String location;
+			
+			// Measurement (voltage, degree, milliamper, ...)
+			private String measurement;
+				
+			public RestEventDTO() {}
+			
+			public RestEventDTO(String id, String state, String location, String measurement) {
+				this.id = id;
+				this.state = state;
+				this.location = location;
+				this.measurement = measurement;
+				
+			}
+			
+			public String getId() {
+				return id;
+			}
+			
+			public void setId(String id) {
+				this.id = id;
+			}
+			
+			public String getStatus() {
+				return state;
+			}
+			
+			public void setStatus(String status) {
+				this.state = status;
+			}
+			
+			public String getLocation() {
+				return location;
+			}
+			
+			public void setLocation(String location) {
+				this.location = location;
+			}
+			
+			public String getMeasurement() {
+				return measurement;
+			}
+			
+			public void setMeasurement(String measurement) {
+				this.measurement = measurement;
+			}
+}
